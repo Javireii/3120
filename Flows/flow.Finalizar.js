79500d9205f71d2dc74f9ec53d82e5ebbbf96085
Javireii <javier.enriquez@shadesdemexico.com>
2024-03-08 13:24:59 -0600

    node modules

--- Flows/flow.Finalizar.js
+++ Flows/flow.Finalizar.js
@@ -1,6 +1,6 @@
 const { addKeyword, EVENTS } = require("@bot-whatsapp/bot");
 const flowBienvenida = require("./flow.Bienvenida");
-
+//holasi
 module.exports = addKeyword("salir").addAnswer(
   ["Gracias por tu interes", "Para volver a activar el flow escribe *_Hola_*"],
   null,
